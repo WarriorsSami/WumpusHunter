ee75b53312ae02ba391fb48572d896243a43a537
WarriorsSami <sbarbutdica@gmail.com>
2022-03-29 22:58:13 +0300

    refactor(player): replace plain variables with vectors

--- entities/sprites.py
+++ entities/sprites.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from settings import *
+vec = pg.math.Vector2
 
 
 class Player(pg.sprite.Sprite):
@@ -10,55 +11,50 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((TILE_SIZE, TILE_SIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.vx, self.vy = 0, 0
-        self.x = x * TILE_SIZE
-        self.y = y * TILE_SIZE
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILE_SIZE
 
     def get_keys(self):
-        self.vx, self.vy = 0, 0
+        self.vel = vec(0, 0)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
-            self.vx = -PLAYER_SPEED
+            self.vel.x = -PLAYER_SPEED
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
-            self.vx = PLAYER_SPEED
+            self.vel.x = PLAYER_SPEED
         if keys[pg.K_UP] or keys[pg.K_w]:
-            self.vy = -PLAYER_SPEED
+            self.vel.y = -PLAYER_SPEED
         if keys[pg.K_DOWN] or keys[pg.K_s]:
-            self.vy = PLAYER_SPEED
-        if self.vx != 0 and self.vy != 0:
-            self.vx *= 0.7071
-            self.vy *= 0.7071
+            self.vel.y = PLAYER_SPEED
+        if self.vel.x != 0 and self.vel.y != 0:
+            self.vel.x *= 0.7071
+            self.vel.y *= 0.7071
 
     def collide_with_walls(self, dir_str):
         hits = pg.sprite.spritecollide(self, self.game.walls, False)
         if hits:
             if dir_str == 'x':
-                if self.vx > 0:
-                    self.x = hits[0].rect.left - self.rect.width
-                if self.vx < 0:
-                    self.x = hits[0].rect.right
-                self.vx = 0
-                self.rect.x = self.x
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.rect.width
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right
+                self.vel.x = 0
+                self.rect.x = self.pos.x
             if dir_str == 'y':
-                if self.vy > 0:
-                    self.y = hits[0].rect.top - self.rect.height
-                if self.vy < 0:
-                    self.y = hits[0].rect.bottom
-                self.vy = 0
-                self.rect.y = self.y
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.rect.height
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom
+                self.vel.y = 0
+                self.rect.y = self.pos.y
 
     def update(self):
         self.get_keys()
 
-        self.x += self.vx * self.game.dt
-        self.rect.x = self.x
-        if self.collide_with_walls('x'):
-            self.x -= self.vx * self.game.dt
-
-        self.y += self.vy * self.game.dt
-        self.rect.y = self.y
-        if self.collide_with_walls('y'):
-            self.y -= self.vy * self.game.dt
+        self.pos += self.vel * self.game.dt
+        self.rect.x = self.pos.x
+        self.collide_with_walls('x')
+        self.rect.y = self.pos.y
+        self.collide_with_walls('y')
 
 
 class Wall(pg.sprite.Sprite):
